1c33ccc35c42746f66d29290d6122de7af3306e9
Jon Imms <jon.imms@buildertrend.com>
2025-07-08 09:24:06 -0500

    testing an update

--- wp-content/plugins/jonimms-blocks/jonimms-blocks.php
+++ wp-content/plugins/jonimms-blocks/jonimms-blocks.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Jonimms Blocks
  * Description:       Collection of custom Gutenberg blocks.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Jon Imms
  * Requires at least: 6.0
  * Requires PHP:      7.4
