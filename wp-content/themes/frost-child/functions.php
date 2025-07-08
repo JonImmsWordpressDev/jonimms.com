0c89f442d292184ae8120957778262369e909b4d
Jon Imms <jon.imms@buildertrend.com>
2025-07-08 17:28:26 -0500

    enqueue child the stylesheet

--- wp-content/themes/frost-child/functions.php
+++ wp-content/themes/frost-child/functions.php
@@ -1,7 +1,6 @@
 <?php
 // Enqueue Google Fonts if not self-hosting (optional fallback)
-function frost_child_enqueue_fonts(): void
-{
+function frost_child_enqueue_fonts(): void{
     wp_enqueue_style(
         'frost-child-fonts',
         'https://fonts.googleapis.com/css2?family=Raleway:wght@100..900&family=Roboto:wght@100..900&display=swap',
@@ -9,3 +8,12 @@ function frost_child_enqueue_fonts(): void
     );
 }
 add_action( 'wp_enqueue_scripts', 'frost_child_enqueue_fonts' );
+
+add_action( 'wp_enqueue_scripts', function () {
+    wp_enqueue_style(
+        'frost-child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        [],
+        filemtime( get_stylesheet_directory() . '/style.css' )
+    );
+});
